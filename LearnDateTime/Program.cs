2740e130456fd819986702436fea35bd49984096
JeffHason <67012500+KyberJeffHason@users.noreply.github.com>
2021-05-23 17:23:39 +0300

    Добавьте файлы проекта.

---
+++ LearnDateTime/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tut_c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var now = DateTime.Now; // текущая дата.
+            var nowutc = DateTime.UtcNow; // текущая дата в формате UTC.
+            Console.WriteLine(now);
+        }
+    }
+}
