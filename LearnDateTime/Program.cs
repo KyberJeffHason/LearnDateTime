b6df09c35e73731fb645290d0a75f20960e26869
JeffHason <67012500+KyberJeffHason@users.noreply.github.com>
2021-05-23 17:57:08 +0300

    Finished

--- LearnDateTime/Program.cs
+++ LearnDateTime/Program.cs
@@ -10,21 +10,44 @@ namespace tut_c
     {
         static void Main(string[] args)
         {
-            var now = DateTime.Now; // текущая дата.
-            var nowutc = DateTime.UtcNow; // текущая дата в формате UTC.
+            var now = DateTime.Now; // Current date.
+            var nowutc = DateTime.UtcNow; // Current date in UTC format.
             Console.WriteLine(now);
             Console.WriteLine(nowutc);
 
-            var date = new DateTime(2021, 05, 23, 17, 27, 1); // Создание даты по инфе 2021 год, 05 месяц, 23 день, 17 часов,27 минут, 1 секунда 
+            var date = new DateTime(2021, 05, 23, 17, 27, 1); // Creating date from this info: 2021 year, 05 month, 23 day, 17 hour, 27 minute, 1 second.
             Console.WriteLine(date);
 
             var usaDate1 = "05-23-2021";
-            var usaDate2 = "05/23/2021";
+            var usaDate2 = "05/24/2021";
+            var dateString = "202105";
+            DateTime date3;
+
 
             var usaDate1_d = DateTime.ParseExact(usaDate1, "MM-dd-yyyy", null);
             Console.WriteLine($"USA FIRST DATE Converted: {usaDate1_d}, from {usaDate1} ");
             var usaDate2_d = DateTime.ParseExact(usaDate2, "MM/dd/yyyy", null);
             Console.WriteLine($"USA SECOND DATE Converted: {usaDate2_d}, from {usaDate2} ");
+
+            var foo = DateTime.Compare(usaDate1_d, usaDate2_d); // -1 means earlier, 0 means the same, 1 means later
+            Console.WriteLine($"Comparing number: {foo}");
+
+            if (DateTime.TryParseExact(dateString, "yyyyMM", null, System.Globalization.DateTimeStyles.None, out date3))
+            {
+                Console.WriteLine($"Conversion was succesful, date {date3}");
+            }
+            else
+            {
+                Console.WriteLine("Conversion failed");
+            }
+
+            var difference = usaDate2_d - usaDate1_d;
+            Console.WriteLine(difference); // same as SpanOfDay
+            var SpanOfDay = TimeSpan.FromDays(1);
+            Console.WriteLine(SpanOfDay); // same as difference
+
+            Console.WriteLine(usaDate2_d - TimeSpan.FromDays(5)); // usaDate2_d - 5 days.
+
         }
     }
 }
